949f279818895ba7a6a3d7e70874a97572ff579c
Leanza Armamento <larmamento@mavs.coloradomesa.edu>
2024-01-25 09:48:43 -0700

    Our first hello world program

---
+++ hello.cpp
@@ -0,0 +1,17 @@
+/*
+Name: Leanza Armamento
+
+First Hello World Program
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // This will print out hello world to the console
+    cout << "Hello World" << endl;
+    cout << "My name is Alpha Intelligence" << endl;
+    cout << "No one understands how hard it is to be an AI" << endl;
+    return 0; // This exits the program with value 0
+}
