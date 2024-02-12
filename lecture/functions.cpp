ac1ec4fe975404c486f9c33f5388cf4ff908a104
Leanza Armamento <larmamento@mavs.coloradomesa.edu>
2024-02-12 09:48:44 -0700

    more notes

---
+++ lecture/functions.cpp
@@ -0,0 +1,72 @@
+/*
+Name: Leanza Armamento
+
+Functions
+*/
+#include <iostream>
+
+using namespace std;
+
+//void sayHello()
+int addToNumber
+{ 
+    //cout << "Hello World" << endl;
+
+    int someNumber;
+    int anotherNumber;
+
+    cout << "Please enter a number: ";
+    cin >> someNumber;
+    //somenumber = 42;
+    result = someNumber + anotherNumber;
+    return someNumber;
+}
+chargiveMeAChar()
+{
+    return 74;
+}
+
+string giveMeAString()
+{
+    return "Leanza";
+}
+
+int addNums(int n1, int n2)
+{
+    //int result;
+    //result = n1+n2;
+    num1= 10000;
+    num2= 30000;
+    return result;
+}
+int main()
+{
+    int answer;
+    int num1, num2;
+
+    cout << "Please enter two numbers seperated by a space: "
+    cin >> num1 >> num2;
+
+    answer = addNums(num1, num2, "");
+    cout << num1 << " + " << num2 << " = " << answer << endl;
+    //int result;
+    //result = addToNumber(); 
+    //cout << "addToNumber: " << addToNumber() << endl;
+
+    //result = addToNumber(); 
+    //cout << "someToNumber: " << addToNumber() << endl;
+     
+    //int result;
+    //result = addToNumber()
+    //cout << result
+
+    //sayHello();
+    //sayHello();
+    //sayHello();
+    //sayHello();
+    //sayHello();
+
+    //string someStr;
+    //getline (cin, someStr);
+    return 0;
+}
