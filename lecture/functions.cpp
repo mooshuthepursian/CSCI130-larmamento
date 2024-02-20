60ef6ee9d5632a808dcaa82698a4e0c841993209
Leanza Armamento <larmamento@mavs.coloradomesa.edu>
2024-02-20 09:16:44 -0700

    notes

--- lecture/functions.cpp
+++ lecture/functions.cpp
@@ -7,48 +7,145 @@ Functions
 
 using namespace std;
 
+//void sayGoodbye();
+//void sayHello();
+
+
+//int addNums(int,int);
+//float addNums(float,float);
+//float addNums(int,float);
+//float addNums(float,int);
+//int multiplyNums(int, int);
+//float avgNums(int&, int&);
+
+void printNums(int&, int&);
+//void getNums(nt&, int&);
+
+int main()
+{
+    int num1 = 0, num2 = 0;
+
+    // getNums(num1, num2);
+
+    //cout << "num1:" << num1 << "num2:" << num2 << endl;
+    num1 = 42;
+    num2 = 15;
+    cout << "In main: &num1: " << &num1 << endl;
+    cout << "In main: &num2; " << &num2 << endl;
+    printNums(num1, num2); 
+    cout << "In main: num1:" << num1 << ""
+    //int num1;
+    //int num2;
+    //float result;
+
+    //cout << "Enter two numbers seperated by a space: ";
+    //cin >> num1 >> num2;
+    
+    //result = addNums <float, float, int>(num1,num2);
+    //cout << num1<< " + " << num2 << " = " << addNums(num1, num2) << endl;
+    //sayHello();
+    //sayGoodbye();
+    //return 0;
+}
+//template <class T1, class T2, class T3>
+//T1 addNums(T2 n1, T3 n2)
+{
+    //return n1 + n2;
+}
+//int addNums(int n1 +n2)
+{ 
+    //cout << "inside int funcion" << endl;
+    //return n1+n2;
+}
+//float addNums(float n1, float n2)
+{
+    //cout << "inside float function" << endl;
+    //return n1+n2;
+}
+//float addNums(int n1, float n2)
+{
+    //cout << "inside int/float function" << endl;
+    //return n1+n2
+}
+//float addNums(int n1, float n2)
+{
+    //cout << "inside float/int function" << endl;
+    //return n1+n2
+}
+//float addNums(float n1, int n2)
+
+
 //void sayHello()
-int addToNumber
+{
+    //cout << "Hello" << endl;
+    //sayGoodbye();
+}
+
+//void sayGoodbye()
+{
+    //cout << "Goodbye" << endl;
+    //sayHello();
+}
+
+//int main()
+{
+    //sayHello();
+    //sayGoodbye();
+    //return 0;
+}
+
+//void sayHello(string name)
+{
+    //string lName;
+    //cout << "enter your last name: ";
+    //cin >> lName;
+    //cout <<"Hello" << name << " " << lName << endl;
+}
+//int addToNumber
 { 
     //cout << "Hello World" << endl;
 
-    int someNumber;
-    int anotherNumber;
+    //int someNumber;
+    //int anotherNumber;
 
-    cout << "Please enter a number: ";
-    cin >> someNumber;
+    //cout << "Please enter a number: ";
+    //cin >> someNumber;
     //somenumber = 42;
-    result = someNumber + anotherNumber;
-    return someNumber;
+    //result = someNumber + anotherNumber;
+    //return someNumber;
 }
-chargiveMeAChar()
+//chargiveMeAChar()
 {
-    return 74;
+    //return 74;
 }
 
-string giveMeAString()
+//string giveMeAString()
 {
-    return "Leanza";
+    //return "Leanza";
 }
 
-int addNums(int n1, int n2)
+//int addNums(int n1, int n2, string name, char someCh)
 {
     //int result;
     //result = n1+n2;
-    num1= 10000;
-    num2= 30000;
-    return result;
+    //num1= 10000;
+    //num2= 20000;
+    //return result;
 }
-int main()
+//int main()
 {
-    int answer;
-    int num1, num2;
+    //sayHello("Leanza");
+    //int answer;
+    //int num1, num2;
 
-    cout << "Please enter two numbers seperated by a space: "
-    cin >> num1 >> num2;
+    //cout << "Please enter two numbers seperated by a space: "
+    //cin >> num1 >> num2;
 
-    answer = addNums(num1, num2, "");
-    cout << num1 << " + " << num2 << " = " << answer << endl;
+    //answer = addNums(num1, num2, "", ' ');
+    //cout << num1 << " + " << num2 << " = " << answer << endl;
+    //cout << "Press enter to quit";
+    //cin,get();
+
     //int result;
     //result = addToNumber(); 
     //cout << "addToNumber: " << addToNumber() << endl;
@@ -68,5 +165,4 @@ int main()
 
     //string someStr;
     //getline (cin, someStr);
-    return 0;
 }
