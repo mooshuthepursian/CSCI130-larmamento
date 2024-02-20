8481e4f07c20c6fe9b8481167720fc47fbea3367
Leanza Armamento <larmamento@mavs.coloradomesa.edu>
2024-02-20 12:38:01 -0700

    2/21/24 notes in class

--- lecture/conditionals.cpp
+++ lecture/conditionals.cpp
@@ -8,7 +8,7 @@ conditionals
 using namespace std;
 
 int main()
-{
+//{
     // cout << boolalpha << (42 == 42) << endl;
     // cout << boolalpha << (42 != 41) << endl;
     // cout << boolalpha << (42 > 42) << endl;
@@ -18,13 +18,16 @@ int main()
 
     // cout << boolalpha << !(42 != 42) << endl; 
     // cout << boolalpha << !(42 == 42) << endl;
-     int num1;
-     bool result; 
 
-      cout << "Enter a number: ";
-      cin >> num1;
+      int num1;
+      bool result; 
 
-      result = isEvenandNegative(num1);
+       cout << "Enter a number: ";
+       cin >> num1;
+       cin.get();
+
+       result = isEvenandNegative(num1);
+
 
     // if(num1 != num2)
     // {
@@ -76,20 +79,45 @@ int main()
     //     cout << "<" << num2 << endl;
     // }
 
-    cout << "After if" << endl;
-    return 0; 
-}
+//     cout << "After if" << endl;
+//     return 0; 
+// }
 
 bool isEvenandNegative(int num1)
 {
-    if (num1%2 != 0)
+    if((num1%2 == 0 && num1 > 0) || (num1 == 0))
     {
-        cout << num1 << "is odd" << endl;
+        cout << num1 << " is even and positive. " << endl;
+        //cout << num1 << " is odd or negative or both." << endl;
     }
-    else
+    else 
     {
-        cout << num1 << "is even" << endl;
+        //cout << num1 << " is odd and negative." << endl;
+        cout << num1 << "is odd or negative or both." << endl;
+        //cout << num1 << " is even and positive." << endl;
     }
+    // string name;
+
+    // cout << "Enter your name: ";
+    // getline(cin, name);
+    // if (num1%2 == 0)
+    // {
+    //     cout << name << " your number " << num1 << " is even." << endl;
+    // }
+    // else
+    // {
+    //     cout << name << " your number " << num1 << " is odd." << endl;
+    // }
+}
+// {
+    // if (num1%2 != 0)
+    // {
+    //     cout << num1 << "is odd" << endl;
+    // }
+    // else
+    // {
+    //     cout << num1 << "is even" << endl;
+    // }
     if (num1 > 0) 
     {
         cout << num1 << "is positive" << endl;
@@ -102,5 +130,5 @@ bool isEvenandNegative(int num1)
     {
         cout << num1 << "is negative" << endl;
     }
-    return true; 
-}
+     return true; 
+//}
