878597db3671d6f3b0c1621f9b1be6f17c810aa4
Leanza Armamento <larmamento@mavs.coloradomesa.edu>
2024-03-04 09:21:53 -0700

    notes

---
+++ lecture/pointers.cpp
@@ -0,0 +1,35 @@
+/* 
+Leanza Armamento
+
+POINTERS!!!!!!!!!!!!!!!!
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1;
+    int* ptr1;
+    int** ptr2;
+    // double* ptr2;
+    //void* ptr42; 
+
+    num1 = 42
+    ptr1= &num1;
+    ptr2= &ptr1;
+
+    cout << "&num1:\t" << &num1 << endl;
+    cout << "ptr1:\t" << ptr1 << endl;
+    cout << "&ptr1:\t" << &ptr1 << endl;
+    cout << "ptr2:\t" << ptr2 << endl;
+    cout << "&ptr2:\t" << &ptr2 << endl;
+
+    // cout << "num1: " << num1 << endl;
+    // cout << "ptr1: " << *ptr1 << endl;
+
+    // cout << "&num1:\t" << &num1 << endl;
+    // cout << "ptr1;\t" << ptr1 << endl;
+    // cout << "&ptr1:\t" << &ptr1 << endl;
+    return 0
+}
