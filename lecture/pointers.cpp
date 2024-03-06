6f6886bed3294ca29efba5593315adb317d50882
Leanza Armamento <larmamento@mavs.coloradomesa.edu>
2024-03-06 09:47:40 -0700

    3/6/24 notes

--- lecture/pointers.cpp
+++ lecture/pointers.cpp
@@ -7,31 +7,31 @@ POINTERS!!!!!!!!!!!!!!!!
 #include <string>
 
 using namespace std;
+void addNums(int*, int*);
 
 int main()
 {
-    int* num1;
-    num1 = new int;
-    string* str1 = new string;
-    long* long1 = new long(8888888888);
+    // num1 = new int;
+    // string* str1 = new string;
+    // long* long1 = new long(8888888888);
 
-    *num1=42;
-    cout << "&num1: \t" << &num1 << endl;
-    cout << "num1:\t " << num1 << endl;
-    cout << "*num1:\t " << *num1 << endl;
+    // *num1=42;
+    // cout << "&num1: \t" << &num1 << endl;
+    // cout << "num1:\t " << num1 << endl;
+    // cout << "*num1:\t " << *num1 << endl;
 
-    *str1 = "Leanza";
-    cout << "&str1: \t" << &str1 << endl;
-    cout << "str1: \t" << str1 << endl;
-    cout << "*str: \t" << *str1 << endl;
+    // *str1 = "Leanza";
+    // cout << "&str1: \t" << &str1 << endl;
+    // cout << "str1: \t" << str1 << endl;
+    // cout << "*str: \t" << *str1 << endl;
 
-    cout << "&long1: \t" << &long1 << endl;
-    cout << "long1: \t" << long1 << endl;
-    cout << "*long: \t" << *long1 << endl;
+    // cout << "&long1: \t" << &long1 << endl;
+    // cout << "long1: \t" << long1 << endl;
+    // cout << "*long: \t" << *long1 << endl;
 
-    delete str1; 
-    delete num1;
-    delete long1;
+    // delete str1; 
+    // delete num1;
+    // delete long1;
     // int* numbers;
     // numbers = new int[1000000];
 
@@ -146,5 +146,34 @@ int main()
     // cout << "&num1:\t" << &num1 << endl;
     // cout << "ptr1;\t" << ptr1 << endl;
     // cout << "&ptr1:\t" << &ptr1 << endl;
+
+    int n1, n2;
+    int* ptr1;
+    int* ptr2;
+
+    ptr1 = &n1;
+    ptr2 = &n2;
+
+    cout << "Enter two numbers: ";
+    cin >> *ptr1 >> *ptr2;
+
+    cout << "ptr1: " << ptr1 << endl;
+    cout << "ptr2: " << ptr2 << endl;
+    addNums(&ptr1, &ptr2);
+
+    cout << "n1: " << n1 << endl;
+    cout << "n2: " << n2 << endl;
+    
     return 0
+}
+
+void addNums(int** num1, int** num2);
+{
+    cout << "num1: " << num1 << endl;
+    cout << "num2: " << num2 << endl;
+    cout << **num1 << "+" << **num2 << "=" 
+         << **num1 + **num2 << endl;
+
+    **num1 = 9000;
+    **num2 = 10000;
 }
