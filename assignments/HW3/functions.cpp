18a2dcae6745b12acb67979fbc5cf39cd78dc309
Leanza Armamento <larmamento@mavs.coloradomesa.edu>
2024-03-11 11:36:36 -0600

    adding nums

--- assignments/HW3/functions.cpp
+++ assignments/HW3/functions.cpp
@@ -7,30 +7,35 @@ A Basic Calculator Using Functions and Automated Unit Testing
 #include <iostream>
 using namespace std;
 void printNums(int&, int&);
-float addNums(int&,int&);
-// template<typename T>
-// T addNums(T num1 + T num2);
+//float addNums(int&,int&);
+template<typename T>
+T addNums(T, T);
 //int multiplyNums(int, int);
 //float avgNums(int&, int&);
-void getNums(int&, int&);
+template<typename T>
+void getNums(T&, T&);
 
-float result;
+
+
 
 int main ()
 {
-    int addNums;
     int num1 = 0, num2= 0;
 
-    getNums(num1, num2);
-    cout << "Enter two numbers seperated by a space: ";
-    cin >> num1 >> num2;
-    result = addNums(num1,num2);
-    cout << num1<< " + " << num2 << " = " << addNums(num1, num2) << endl;
+    getNums<int>(num1, num2);
+    //cout << "Enter two numbers seperated by a space: ";
+    //cin >> num1 >> num2;
+    cout << num1<< " + " << num2 << " = "  << addNums<int>(num1,num2) << endl;
 
     return 0;
 }
-// template<typename T>
-// T addNums(T num1 + T num2) { 
-//     cout << "inside int funcion" << endl;
-//     return num1+num2;
-// }
+
+template <typename T> T addNums(T num1, T num2)
+{
+    return num1 + num2;
+}
+template <typename T> void getNums(T &num1, T &num2)
+{
+    cout << "Enter two numbers seperated by a space" << endl;
+    cin >> num1 >> num2;
+}
