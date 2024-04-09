f0e2334b934356fbf5db91b600e9bc3414ae102f
Leanza Armamento <larmamento@mavs.coloradomesa.edu>
2024-04-09 09:37:15 -0600

    4/9/24 notes

---
+++ lecture/structs.cpp
@@ -0,0 +1,83 @@
+/*
+Name: Leanza Armamento
+Structures
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Data
+{
+    string fName; 
+    string lName;
+    string title;
+    string color;
+    int favNum;
+    int numbers[1];
+
+    int* somePointer;
+}
+
+int main(int argc, char* argv[])
+{
+    // Data* dataGroup[100];
+    
+    // for(int i = 0; i < 100; i++)
+    // {
+    //     dataGroup[i] = nullptr;
+    // }
+
+    // for(int i=0; i < 5; i++)
+    // {
+    //     dataGroup[i] = new Data;
+    //     cin >> dataGroup[i]->fName;
+    //     cin >> dataGroup[i]->lName;
+    //     cin >> dataGroup[i]->title;
+    // }
+
+    // for(int i=0; i < 5; i++)
+    // {
+    //     cout << dataGroup[i]->fName << "\t"
+    //          << dataGroup[i]->lName << "\t"
+    //          << dataGroup[i]->title << endl;
+    // }
+    // Data* dataGroup[5];
+    // Data dataGroup1[5];
+
+    // Data* myData1 = new Data;
+    // Data* myData2;
+    // Data someData;
+    // Data thirdData;
+
+    // myData2 = &someData;
+
+    // dataGroup[0] = myData1;
+    // dataGroup[1] = myData2;
+    // dataGroup[2] = &thirdData;
+
+    // dataGroup2[0] = *myData1;
+    // dataGroup2[1] = *myData
+    // // cout << "sizeof(Data): " << sizeof(Data) << endl;
+    // // Data myData1;
+
+    // myData2->fName = "Jeremy";
+
+    // (*myData1).fName = "Leanza";
+    // myData1->lName = "Armamento";
+    // myData1->title = "Student";
+    // myData1->color = "Red";
+    // myData1->favNum = 3;
+    // myData1->numbers[0] = 3;
+    // myData1->numbers[1] = 2;
+    // myData1->numbers[2] = 1;
+
+    // myData1->somePointer = &myData1-> favNum;
+
+    // cout << myData1.fName << "\t" << myData1.lName << endl;
+    // delete myData1; 
+    for(int i=0; i < 5; i++)
+    {
+        delete
+    }
+    return 0;
+}
