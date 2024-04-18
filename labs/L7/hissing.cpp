f31883509b11cf550951df579b8e53d191eecfdd
Leanza Armamento <larmamento@mavs.coloradomesa.edu>
2024-04-18 09:48:49 -0600

    first try

--- labs/L7/hissing.cpp
+++ labs/L7/hissing.cpp
@@ -35,13 +35,19 @@ string answer(const string &line) {
     // implment algorithm step 2
     // return "hiss" if ss is found in line
     // otherwise, return "no hiss"
-    return "hiss";
+    if ()
+        return "hiss";
+    else
+        return "no hiss";
+    return 0;
 }
 
 // unit testing answer()
 void testAnswer() {
     // FIXME4
     // write at least two test cases to test answer()
+    assert(answer("hiss") == "hiss");
+    assert(answer("nohiss") == "no hiss");
     cerr << "All test cases passed!\n";
 }
 
@@ -51,5 +57,6 @@ void solve() {
     // string consists of only lowercase letters (no spaces) upto 30 chars
     // FIXME5
     // read string into line
+    cin >> line;
     cout << answer(line) << endl;
 }
