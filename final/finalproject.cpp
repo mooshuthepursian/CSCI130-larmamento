ea5f89263670693ca1c1017a64540db70e8cd146
Leanza Armamento <larmamento@mavs.coloradomesa.edu>
2024-05-14 18:00:54 -0600

    almost done

--- final/finalproject.cpp
+++ final/finalproject.cpp
@@ -15,16 +15,214 @@ Steps:
 */
 
 #include <iostream>
-#include <algorithm> 
-#include <functional> 
+#include <fstream>
+#include <string>
 #include <vector>
-#include <cassert>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
+
+void displayHangman(int wrongGuesses) 
+{
+    if (wrongGuesses == 0) 
+    {
+        cout << "Stage 1" << endl;
+        cout << "______" << endl;
+        cout << "|     " << endl;
+        cout << "|     " << endl;
+        cout << "|     " << endl;
+        cout << "|     " << endl;
+        cout << "------" << endl; 
+    } 
+    else if (wrongGuesses == 1) 
+    {
+        cout << "Stage 2" << endl;
+        cout << "______" << endl;
+        cout << "|  0  " << endl;
+        cout << "|     " << endl;
+        cout << "|     " << endl;
+        cout << "|     " << endl;
+        cout << "------" << endl; 
+    } 
+    else if (wrongGuesses == 2) 
+    {
+        cout << "Stage 3" << endl;
+        cout << "______" << endl;
+        cout << "|  0  " << endl;
+        cout << "|  |  " << endl;
+        cout << "|     " << endl; 
+        cout << "|     " << endl;
+        cout << "------" << endl;
+    } 
+    else if (wrongGuesses == 3) 
+    {
+        cout << "Stage 4" << endl; 
+        cout << "______" << endl;
+        cout << "|  0  " << endl;
+        cout << "| /|  " << endl;
+        cout << "|     " << endl; 
+        cout << "|     " << endl;
+        cout << "------" << endl;
+    } 
+    else if (wrongGuesses == 4) 
+    {
+        cout << "Stage 5" << endl;
+        cout << "______" << endl;
+        cout << "|  0  " << endl;
+        cout << "| /|\\ " << endl;
+        cout << "|     " << endl; 
+        cout << "|     " << endl;
+        cout << "------" << endl;
+    } 
+    else if (wrongGuesses == 5) 
+    {
+        cout << "Stage 6" << endl; 
+        cout << "______" << endl;
+        cout << "|  0  " << endl;
+        cout << "| /|\\ " << endl;
+        cout << "| /   " << endl; 
+        cout << "|     " << endl;
+        cout << "------" << endl;
+    } 
+}
+vector<string> readWordsFromFile(const string& filename) //help from tlc center and chat gpt in understanding
+{
+    vector<string> words;
+    ifstream inputFile(filename);
+    string word;
+    while (inputFile >> word)
+    {
+        words.push_back(word);
+    }
+    inputFile.close();
+    return words;
+}
+
+string chooseWord(const vector<string>& words)  
+{
+    srand(static_cast<unsigned int>(time(0)));
+    int randomIndex = rand() % words.size();
+    return words[randomIndex];
+}
+
+string initializeDisplayWord(const string& word) 
+{
+    string displayWord(word.length(), '_');
+    return displayWord;
+}
+
+
+void game() 
+{
+    vector<string> words = readWordsFromFile("words.txt");
+    string word = chooseWord(words);
+    string displayWord = initializeDisplayWord(word);
+    
+    int wrongGuesses = 0;
+    const int maxWrong = 6;
+    vector<char> guessedLetters;
+
+    while (wrongGuesses < maxWrong && displayWord != word) 
+    {
+        displayHangman(wrongGuesses);
+        cout << "Amount you can get wrong remaining: " << maxWrong - wrongGuesses << endl;
+        cout << "Word: " << displayWord << endl;
+        char guess;
+        cout << "Enter your guess: ";
+        cin >> guess;
+        bool correctGuess = false;
+        for (size_t i = 0; i < word.size(); ++i) 
+        {
+            if (word[i] == guess) 
+            {
+                if (displayWord[i] == '_') 
+                {
+                    displayWord[i] = guess;
+                    correctGuess = true;
+                } 
+                else 
+                {
+                    cout << "You've already guessed that letter!" << endl;
+                }
+            }
+        }
+        if (!correctGuess) 
+        {
+            ++wrongGuesses;
+        }
+    }
+
+    if (displayWord == word) 
+    {
+        cout << "Congratulations! You guessed the word: " << word << endl;
+        cout << "Will you get it next time though??" << endl;
+    } 
+    else 
+    {
+        cout << "Stage 7" << endl;
+        cout << "______" << endl;
+        cout << "|  0  " << endl;
+        cout << "| /|\\ " << endl;
+        cout << "| / \\ " << endl; 
+        cout << "|     " << endl;
+        cout << "------" << endl;
+        cout << "NOOOOOOOOOOO you ran out of guesses, so you lose :(" << endl;
+        cout << "The word was: " << word << endl;
+    }
+}
+
 int main()
 {
+    string playGameAgain;
+    string fName;
     cout << "Hello! What is your name?" << endl;
-    cin >> fName << endl;
-    cout << "Hello" << fName << "." << "Welcome to my hangman game. To play the game players must guess letters to uncover a hidden word within a limited number of attempts, with each incorrect guess resulting in the drawing of a segment of a gallows until the word is guessed correctly or the gallows is completed." << endl;
+    cin >> fName;
+    cout << "Hello, " << fName << ". Welcome to my hangman game. To play the game, players must guess letters to uncover a hidden word within a limited number of attempts, with each incorrect guess resulting in the drawing of a segment of a gallows until the word is guessed correctly or the gallows is completed." << endl;
+    string difficulty;
+    cout << "How difficult do you want the game to be? Options: Easy, Medium, Hard, Impossible) ";
+    cout << "Easy: consists of 3 letter words."
+    cout << "Medium: consists of 5 letter words."
+    cout << "Hard: consists of 7 letter words."
+    cout << "Impossible: consists of 10+ letter words."
+    cout << "Type the level you want:"
+    cin >> difficulty;
+
+    string filename;
+    if (difficulty == "Easy") 
+    {
+        filename = "easy.txt";
+    } 
+    else if (difficulty == "Medium") 
+    {
+        filename = "medium.txt";
+    } 
+    else if (difficulty == "Hard") 
+    {
+        filename = "hard.txt";
+    } 
+    else if (difficulty == "Impossible") 
+    {
+        filename = "impossible.txt";
+    } 
+    else 
+    {
+        cout << "Invalid difficulty level. Exiting...";
+        return 1;
+    }
+
+    vector<string> words = readWordsFromFile(filename);
+    // Proceed with the rest of your game logic...
+}
+
+    do 
+    {
+        game();
+        cout << "Do you want to play the game again? (y/n): ";
+        cin >> playGameAgain;
+    }
+    while (playGameAgain == "y" || playGameAgain == "Y");
+    
+    return 0;
 }
